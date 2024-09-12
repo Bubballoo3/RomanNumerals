21aee514effac65c3b791636ab128d77a2d4b240
Braeden <singleton1@kenyon.edu>
2024-09-12 02:25:43 -0400

    faulty but error free version of intToNumeral

--- Roman.cpp
+++ Roman.cpp
@@ -7,16 +7,20 @@
 using namespace std;
 
 Roman::Roman(const std::string& numeral) : romanNumeral(numeral),integerValue(romanToInt(romanNumeral)) {}
+Roman::Roman(int value) : romanNumeral(intToRoman(value)),integerValue(value) {}
+Roman::Roman() : romanNumeral(0),integerValue(0) {}
+//Private Methods
 
 const map<char,int> Roman::RnMap= {
-    {'I',1},
-    {'V',5},
-    {'X',10},
-    {'L',50},
-    {'C',100},
+    {'M',1000},
     {'D',500},
-    {'M',1000}
+    {'C',100},
+    {'L',50},
+    {'X',10},
+    {'V',5},
+    {'I',1},
 };
+
 int Roman::romanToInt(const std::string& numeral) {
     // std::map<char, int> evals;
     // evals['I'] = 1;
@@ -35,9 +39,9 @@ int Roman::romanToInt(const std::string& numeral) {
         }
         int currentval;
         const int firstValue = RnMap.find(current)->second;
-        cout << "First value is: " << firstValue << endl;
+        // cout << "First value is: " << firstValue << endl;
         const int nextValue = RnMap.find(next)->second;
-        cout << "Second value is: " << nextValue << endl;
+        // cout << "Second value is: " << nextValue << endl;
         // Check relative size of the next value
         if(firstValue < nextValue){
             // If the next value is larger, we need to consider it 
@@ -45,6 +49,7 @@ int Roman::romanToInt(const std::string& numeral) {
             i++;
         }
         else {
+            //if not, we use the first value normally
             currentval=firstValue;
         }
         sum+=currentval;
@@ -52,7 +57,48 @@ int Roman::romanToInt(const std::string& numeral) {
     return sum;
 };
 
+string Roman::intToRoman(int value){
+    int remainder=value;
+    string numeral;
+
+    while(remainder > 0){
+        map<char, int>::const_iterator it = Roman::RnMap.begin();
+        while(it != RnMap.end()){
+            int numChar=it->first;
+            int value=it->second;
+            while(remainder >= value){
+                numeral+=numChar;
+                remainder=remainder-value;
+                cout << "value: " << remainder << "Numeral: "<< numeral << endl;
+            }
+            int gap=value-remainder;
+            map<char, int>::const_iterator next=std::next(it,2);
+            if(gap < (next->second)){
+                numeral=numeral.substr(0,numeral.length()-1);
+                numeral+= next->first + numChar;
+
+            }
+            ++it; 
+        }
+    }
+
+    return numeral;
+}
+
+//Public Methods
 
 void Roman::printDec() const {
     std::cout << integerValue << std::endl;
 };
+
+void Roman::printRoman() const {
+    std::cout << romanNumeral << std::endl;
+}
+
+int Roman::getInt() const {
+    return integerValue;
+}
+
+string Roman::getRoman() const {
+    return romanNumeral;
+}
